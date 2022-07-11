09462dec8400b6b5965aa55276f76414285fe850
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-11 12:14:45 -0500

    service worker code ie; eventlisteners and e.wait

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,11 @@
+const APP_PREFIX = 'budget-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "/index.html",
     "/public/css/style.css",
     "/public/js/index.js",
-    "/assets/js/db.",
+    "/assets/js/db.js",
     './manifest.json',
     '.public/icons/icon-512x512.png',
     '.public/icons/icon-384x384.png',
@@ -12,4 +15,46 @@ const FILES_TO_CACHE = [
     '.public/icons/icon-128x128.png',
     '.public/icons/icon-96x96.png',
     '.public/icons/icon-72x72.png'
-]
+];
+// respond with cached resources
+self.addEventListener('install', function(e){
+    e.waitUntill(
+        caches.open(CACHE_NAME).then(function (cache){
+            console.log('installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activate', function (e){
+    e.waitUntill(
+        caches.keys().then(function (keyList){
+            let cacheKeeplist = keyList.filter(function (key){
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i){
+                if (cacheKeeplist.indexOf(key) === -1){
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+});
