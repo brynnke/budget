788b3c6165352585ec47cd894e31c1846ad02280
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-11 12:00:13 -0500

    working on updating service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,15 @@
 const FILES_TO_CACHE = [
-    "/",
     "/index.html",
     "/public/css/style.css",
     "/public/js/index.js",
     "/assets/js/db.",
     './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    '.public/icons/icon-512x512.png',
+    '.public/icons/icon-384x384.png',
+    '.public/icons/icon-192x192.png',
+    '.public/icons/icon-152x152.png',
+    '.public/icons/icon-144x144.png',
+    '.public/icons/icon-128x128.png',
+    '.public/icons/icon-96x96.png',
+    '.public/icons/icon-72x72.png'
 ]
